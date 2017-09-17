34ffad1303b5a3b952893ccba4c9c3a7b6932680
krubot <krubot.ops@gmail.com>
2017-09-17 22:10:32 +0100

    fix comments in main and add vendor dependencies

--- main.go
+++ main.go
@@ -11,19 +11,19 @@ import (
 
 func main() {
 
-	# heroku sets the PORT environment variable      
+	// heroku sets the PORT environment variable      
         port := os.Getenv("PORT")
         if port == "" {
         	log.Fatal("$PORT must be set")
 	}
 
-	# configure gin router with webpage html
+	// configure gin router with webpage html
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("index.html")
 	router.GET("/", func( content *gin.Context) {
-		content.HTML(http.StatusOk,"index.html",nil)
+		content.HTML(http.StatusOK,"index.html",nil)
 	})
 	router.Run(":"+port)
 
